234082ffc27be09e16dcc22076d36038a940a114
Alexander Fountain <alexanderfountain@gmail.com>
2021-10-20 21:24:37 -0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,46 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import "../components/scss/coming.scss"
+import LogoImage from "../images/logo.png"
+import MboWords from "../images/mbo-words.png"
+import RollingStones from "../images/logos/rollingstone.png"
+import Newsweek from "../images/logos/newsweek.png"
+import Forbes from "../images/logos/forbes.png"
+import FastCompany from "../images/logos/fastcompany.png"
+import Bizj from "../images/logos/bizj.png"
+
+
+const IndexPage = () => (
+  <div className="container">
+    <div className="coming-top">
+      <div className="coming-top-links">
+        <div className="coming-engage">ENGAGE YOUR AUDIENCE</div>
+        <div className="dot"></div>
+        <div className="coming-skyrocket">SKYROCKET YOUR BRAND</div>
+      </div>
+    </div>
+      <div className="coming-mid">
+        <img className="logo" src={LogoImage} />
+        <img className="mbo-words" src={MboWords} />
+        <div className="coming-soon">
+        <div className="dot"></div>
+        <div className="coming-soon-text">COMING SOON</div>
+        <div className="dot"></div>
+        </div>
+      </div>
+      <div className="coming-bottom">
+        <img className="forbes" src={Forbes} />
+        <img className="bizj" src={Bizj} />
+        <img className="fastcompany" src={FastCompany} />
+        <img className="newsweek" src={Newsweek} />
+        <img className="rollingstones" src={RollingStones} />
+
+      </div>
+    </div>
+)
+
+export default IndexPage
